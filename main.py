b14c8a397fe1ca55ca05f51096eab7a91f36b85d
Camilo Pulido <ocpulidop@unal.edu.co>
2023-01-15 15:44:19 +0100

    Adding queries

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ movies = [
         'id': 1,
         'title': 'Avatar',
         'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
+        'year': 2009,
         'rating': 7.8,
-        'category': 'Acción'    
+        'category': 'action'    
     },
     {
         'id': 2,
         'title': 'Avatar 2',
         'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2012',
+        'year': 2022,
         'rating': 7.8,
-        'category': 'Acción'    
+        'category': 'action'    
     }  
 ]
 
@@ -37,3 +37,15 @@ def get_movie(id:int):
     for item in movies:
         if item['id'] == id:
             return item
+
+
+def valid_querie(year:int,category:str,movie:dict):
+    return movie['category'] == category and movie['year'] == year
+
+@app.get('/movies/', tags=["movies"]) # query parameters
+def get_movies_by_query(category:str,year:int=None):
+    if year is None:
+        return [movie for movie in movies if movie['category'] == category]
+    else:
+        return [movie for movie in movies if valid_querie(year,category,movie)]
+    
