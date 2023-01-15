18bb9e23e90160916fc20422cdb7db502583f9d6
Camilo Pulido <ocpulidop@unal.edu.co>
2023-01-15 15:17:12 +0100

    first get

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = "My app with fastapi"
 app.version = "0.0.1"
 
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    } 
+]
+
 @app.get('/',tags=["home"])
 def message():
-    return "Hello world"
+    return HTMLResponse("<h1>hello<h1/>")
+
+@app.get('/movies',tags=["movies"])
+def get_movies():
+    return movies
