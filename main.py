6f5e9fce6c9e827560857cead60a512fda03f6aa
Camilo Pulido <ocpulidop@unal.edu.co>
2023-01-15 19:56:39 +0100

    Add validations for paths and queries

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from typing import Optional
 
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Path, Query
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 app.title = "My app with fastapi"
@@ -11,11 +11,20 @@ app.version = "0.0.1"
 
 class Movie(BaseModel):
     id: Optional[int] = None
-    title: str
-    overview: str
-    year: str
-    rating: str
-    category: str
+    title: str = Field(default="My title", min_length=5, max_length=20)
+    overview: str = Field(default="My overview", min_length=10, max_length=100)
+    year: int = Field(default=2023, le=2023)
+    rating: float = Field(default=3.0, ge=0.0, le=10)
+    category: str = Field(default="My title", min_length=5, max_length=20)
+
+    class Config:
+        schema_extra = {
+            "title": "my title",
+            "overview": "my overview",
+            "year": 2019,
+            "rating": 7.8,
+            "category": "action",
+        }
 
 
 movies = [
@@ -49,7 +58,7 @@ def get_movies():
 
 
 @app.get("/movies/{id}", tags=["movies"])
-def get_movie(id: int):
+def get_movie(id: int = Path(ge=1, le=2000)):
     for item in movies:
         if item["id"] == id:
             return item
@@ -60,7 +69,7 @@ def valid_querie(year: int, category: str, movie: dict):
 
 
 @app.get("/movies/", tags=["movies"])  # query parameters
-def get_movies_by_query(category: str, year: int = None):
+def get_movies_by_query(category: str=Query(min_length=5, max_length=15), year: int = None):
     if year is None:
         return [movie for movie in movies if movie["category"] == category]
     else:
