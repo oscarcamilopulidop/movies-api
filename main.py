a049368b35c59d81864627bae01a78da58719389
Camilo Pulido <ocpulidop@unal.edu.co>
2023-01-15 15:22:05 +0100

    Adding parameters in queries

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ movies = [
         'year': '2009',
         'rating': 7.8,
         'category': 'Acción'    
-    } 
+    },
+    {
+        'id': 2,
+        'title': 'Avatar 2',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2012',
+        'rating': 7.8,
+        'category': 'Acción'    
+    }  
 ]
 
 @app.get('/',tags=["home"])
@@ -23,3 +31,9 @@ def message():
 @app.get('/movies',tags=["movies"])
 def get_movies():
     return movies
+
+@app.get('/movies/{id}',tags=["movies"])
+def get_movie(id:int):
+    for item in movies:
+        if item['id'] == id:
+            return item
